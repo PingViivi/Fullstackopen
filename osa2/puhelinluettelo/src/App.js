b63999bf9b2a5de818c84b7ee50a800699151fb8
Viivi Tuomi <viivi.tuomi@louhosdigital.fi>
2023-08-04 19:07:49 +0300

    Osan 2 tehtävät 2.6.-2.10

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const person = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(person))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <PersonForm 
+        handleSubmit={handleSubmit}
+        handleNumberChange={handleNumberChange}
+        handleNameChange={handleNameChange}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App
